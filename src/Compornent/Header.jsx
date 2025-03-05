fbddf36d68b7e177d724a28f395d6fd66a9f5234
Adedamola <adedamolaobadimu@gmail.com>
2025-03-05 14:56:29 +0100

    aos update

--- src/Compornent/Header.jsx
+++ src/Compornent/Header.jsx
@@ -1,19 +1,30 @@
 import React from 'react'
 import './Header.css'
+import { useEffect } from "react";
 import { CiLinkedin } from "react-icons/ci";
 import { FaGithub } from "react-icons/fa";
 import { FaTwitter } from "react-icons/fa";
 import { CiInstagram } from "react-icons/ci";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 
 import steady from '../assets/man.png'
 const Header = () => {
+  useEffect(() => {
+    AOS.init({
+      duration: 1000, // Animation duration in milliseconds
+      easing: "ease-in-out", // Type of easing
+      once: true, // Whether animation should happen only once
+      offset: 100, // Offset (in px) before animation starts
+    });
+  }, []);
   return (
     <section className='home' id='home'>
       <div className='home-content'>
-        <h1 className='Adedamola'>Hello, I'm <span>Adedamola</span></h1>
+        <h1 className='Adedamola' data-aos="fade-up">Hello, I'm <span>Adedamola</span></h1>
         <h3 className='text-animation'> I'm a <span className='name'></span></h3>
-        <p>a passionate Front-End Developer dedicated to crafting clean, responsive, and user-friendly web experiences. With a strong foundation in HTML, CSS, JavaScript, and React, I specialize in turning creative ideas into functional and visually appealing websites
+        <p  data-aos="zoom-in">a passionate Front-End Developer dedicated to crafting clean, responsive, and user-friendly web experiences. With a strong foundation in HTML, CSS, JavaScript, and React, I specialize in turning creative ideas into functional and visually appealing websites
         </p>
         <div className='social-icon'>
           <a href="#"><CiLinkedin /></a>
@@ -26,7 +37,7 @@ const Header = () => {
           <a href="#" className='btn'>Contact</a>
         </div>
       </div>
-        <div className='home-img'>
+        <div className='home-img'  data-aos="fade-left">
           <img src={steady} alt=""/>
         </div>
       
